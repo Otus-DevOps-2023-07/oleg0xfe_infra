e31136b26dcf28260f145f1b26b7ca4b5a86b568
Oleg Egorov <oleg_e@protonmail.com>
2023-10-24 06:14:12 +0000

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.95.0"
     }
   }
@@ -9,9 +9,9 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  zone = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
@@ -40,16 +40,16 @@ resource "yandex_compute_instance" "app" {
   }
 
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
     agent = false
     # путь до приватного ключа
     private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
