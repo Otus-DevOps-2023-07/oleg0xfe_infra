6bfd83deb79184ed22c36554db7d87228f87d8dc
Oleg Egorov <oleg_e@protonmail.com>
2023-10-28 21:41:13 +0300

    ДЗ 7

---
+++ terraform/prod/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.95.0"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+module "app" {
+  source           = "../modules/app"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+  app_disk_image   = var.app_disk_image
+  #subnet_id        = var.subnet_id
+  subnet_id        = yandex_vpc_subnet.app-subnet.id
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  #subnet_id       = var.subnet_id
+  subnet_id       = yandex_vpc_subnet.app-subnet.id
+}
+
